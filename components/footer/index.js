511a59780659b86d738b12d48afa97f293dea65f
Hazmi <whitespaces@gmail.com>
2019-05-03 21:51:40 +0700

    add page layout and footer

---
+++ components/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+export const Footer = () => (
+  <footer className="info">
+    <p>Double-click to edit a todo</p>
+    <p>Created by <a href="http://github.com/hazmi/TodoMVC">Hazmi</a></p>
+    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+  </footer>
+);
+
+export default Footer;
---
+++ pages/_app.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import Head from 'next/head'
+import { Footer } from '../components/footer';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Head>
+          <title>TodoMVC</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+        <Footer />
+        <style jsx global>{`
+          html,
+          body {
+            margin: 0;
+            padding: 0;
+          }
+          button {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            background: none;
+            font-size: 100%;
+            vertical-align: baseline;
+            font-family: inherit;
+            font-weight: inherit;
+            color: inherit;
+            -webkit-appearance: none;
+            appearance: none;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+          body {
+            font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            line-height: 1.4em;
+            background: #f5f5f5;
+            color: #4d4d4d;
+            min-width: 230px;
+            max-width: 550px;
+            margin: 0 auto;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            font-weight: 300;
+          }
+          :focus {
+            outline: 0;
+          }
+          .hidden {
+            display: none;
+          }
+      `}</style>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
