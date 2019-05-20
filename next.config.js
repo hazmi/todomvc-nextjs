02597e06c9e6b01c9126be42ebb3b3d454e94f13
Hazmi <whitespaces@gmail.com>
2019-05-21 04:24:01 +0700

    add offline support for pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  target: "serverless"
-})
+const withSass = require('@zeit/next-sass');
+const withOffline = require('next-offline');
+
+const nextConfig = {
+  target: "serverless",
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = withOffline(withSass(nextConfig));
